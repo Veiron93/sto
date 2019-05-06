be6e049382cddec02707fe654c88e68a598856e8
Veiron93 <yidin_93.02@mail.ru>
2019-05-07 00:01:59 +1100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,111 @@
 <template>
-	<div>
-		<router-link to="/">Home</router-link>
-		<router-view/>
-	</div>
+	<header>
+		<div class="left-section">
+			<div class="logo">
+				<a href="">
+					<img src="@/assets/img/logo.png" alt="">
+				</a>
+			</div>
+
+			<div class="cities">
+				<select name="" id="">
+					<option v-for="city in cities" :value="city.code">
+						{{ city.name }}
+					</option>
+				</select>
+			</div>
+		</div>
+
+		<div class="rigth-section">
+			<div class="top-navigation">
+				<router-link to="/">Автосервисы</router-link>
+			</div>
+
+			<div class="btn-menu">
+				<img src="@/assets/img/icons/menu.svg" alt="">
+			</div>
+		</div>	
+	</header>
 </template>
 
 <script>
 	export default {
-		name: 'Header'
+		name: 'Header',
+
+		data() {
+			return{
+				cities: [
+					{name: 'Южно-Сахалинск', code: 'ys'},
+					{name: 'Долинск', code: 'dolinsk'},
+					{name: 'Корсаков', code: 'korsakov'},
+				]
+			}
+		}
 	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="scss">
+	header{
+		width: 100%;
+		max-width: 1140px;
+		padding: 0px 15px;
+		margin: 0px auto;
+		border: 1px solid;
+		height: 60px;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 
+		.left-section, .rigth-section{
+			display: flex;
+			align-items: center;
+		}
+
+		.left-section{
+			.logo{
+				margin-right: 100px;
+				a{
+					img{
+						height: 36px;
+						display: block;
+					}
+				}
+			}	
+		}
+
+		.rigth-section{
+
+			.top-navigation{
+				a{
+					text-transform: uppercase;
+					font-size: 14px;
+					text-decoration: none;
+					color: #000;
+					letter-spacing: .5px;
+				}
+			}
+
+			.btn-menu{
+				margin-left: 50px;
+				border: 1px solid;
+				flex: none;
+				width: 40px;
+				height: 40px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				cursor: pointer;
+
+				img{
+					width: 24px;
+					height: 24px;
+				}
+			}
+		}
+		
+		
+
+		
+	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
 	<div class="home">
 		<div class="container">
 			<div class="row">
-				<div class="col-12">
-
+				<div class="col">
+					<div class="block-sorting"></div>
 					
 				</div>
 			</div>
@@ -20,3 +20,13 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss">
+	.block-sorting{
+		margin-top: 50px;
+		border-radius: 4px;
+		height: 50px;
+		background: $accent;
+		box-shadow: 0px 5px 13.92px 2.08px rgba(17, 127, 212, 0.35);
+	}
+</style>
